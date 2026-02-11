a035580c26059920eaf89e5b56f44b7a5c59c0c8
Matius980 <157426874+Matius980@users.noreply.github.com>
2026-02-10 19:36:33 -0800

    Refactor MediaLib to manage media collections
    Refactored MediaLib class to manage collections of books, movies, and songs. Added checks to prevent duplicate entries and updated the toString method to display all media items.

--- MediaLib.java
+++ MediaLib.java
@@ -1,38 +1,109 @@
+/*
+ * Activity 2.2.9
+ */
+import java.util.ArrayList;
+
 public class MediaLib
 {
-  private Book book;
-  private Movie movie;
-  private Song song;
-
+  public static String owner = "PLTW";
+  
+  private static int numEntries;
+  private static int numBooks;
+  private static int numMovies;
+  private static int numSongs;
+  
+  private ArrayList<Book> books;
+  private ArrayList<Movie> movies;
+  private ArrayList<Song> songs;
+  
+  public MediaLib()
+  {
+    books = new ArrayList<>();
+    movies = new ArrayList<>();
+    songs = new ArrayList<>();
+  }
+  
   public void addBook(Book b)
   {
-    this.book = b;
+    if (!books.contains(b))
+    {
+      books.add(b);
+      numEntries++;
+      numBooks++;
+    } 
+    else
+      System.out.println("Cannot add a new book: \"" + b.getTitle() + "\" by " + b.getAuthor() + " already exists");
   }
-
+  
   public void addMovie(Movie m)
   {
-    this.movie = m;
+    if (!movies.contains(m))
+    {
+      movies.add(m);
+      numEntries++;
+      numMovies++;
+    }
+    else
+      System.out.println("Cannot add a new movie: A movie already exists");
   }
-
+  
   public void addSong(Song s)
   {
-    this.song = s;
+    if (!songs.contains(s))
+    {
+      songs.add(s);
+      numEntries++;
+      numSongs++;
+    }
+    else
+      System.out.println("Cannot add a new song: A song already exists");
   }
 
+  public static String getOwner()
+  {
+    return owner;
+  }
 
-  public String toString()
+  public static void changeOwner(String o)
+  {
+    owner = o;
+  }
+  
+  public static int getNumEntries()
+  {
+    return numEntries;
+  }
+  
+  public static int getNumBooks()
+  {
+    return numBooks;
+  }
+  
+  public static int getNumMovies()
+  {
+    return numMovies;
+  }
+  
+  public static int getNumSongs()
+  {
+    return numSongs;
+  }
+
+  public int getTotalEntries() {
+    return numEntries;
+  }
+  
+  public String toString() 
   {
     String info = "";
-
-    if (this.book != null)
-      info += "Book: " + this.book;
-
-    if (this.movie != null)
-      info += "\nMovie: " + this.movie;
-
-    if (this.song != null)
-      info += "\nSong: " + this.song;
-
+    
+    for (Book b : books)
+      info += "Book: " + b + "\n";
+    for (Movie m : movies)
+      info += "Movie: " + m + "\n";
+    for (Song s : songs)
+      info += "Song: " + s + "\n";
+ 
     return info;
   }
 }
